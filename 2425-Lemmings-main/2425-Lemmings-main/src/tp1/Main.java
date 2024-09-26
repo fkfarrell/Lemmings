d2a91d441e144214a380b871c7b56d9b0c4d02a5
Finn <fifarrel@tcd.ie>
2024-09-26 16:34:45 +0200

    Finn testing repo connection

--- 2425-Lemmings-main/2425-Lemmings-main/src/tp1/Main.java
+++ 2425-Lemmings-main/2425-Lemmings-main/src/tp1/Main.java
@@ -22,17 +22,21 @@ public class Main {
 		// You can replace the following line by the previous line if using Java21
 		Locale.setDefault(new Locale("es", "ES"));
 		try {
-			
+
 			int nLevel = 1;
-			if (args.length != 0) nLevel = Integer.parseInt(args[0]);
+			if (args.length != 0)
+				nLevel = Integer.parseInt(args[0]);
 
 			Game game = new Game(nLevel);
 			GameView view = new ConsoleView(game);
 			Controller controller = new Controller(game, view);
-					
+
 			controller.run();
 		} catch (NumberFormatException e) {
 			System.out.println(String.format(Messages.LEVEL_NOT_A_NUMBER_ERROR, args[0]));
 		}
+
+		// Finns git test
+
 	}
 }
