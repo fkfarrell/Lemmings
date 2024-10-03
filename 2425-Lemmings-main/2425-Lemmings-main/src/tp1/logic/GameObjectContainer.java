4833b3fd19447ef6c54580824ef7773e7ff82393
kaoreill <kaoreill@ucm.es>
2024-10-03 13:40:09 +0100

    Update GameObjectContainer.java

--- 2425-Lemmings-main/2425-Lemmings-main/src/tp1/logic/GameObjectContainer.java
+++ 2425-Lemmings-main/2425-Lemmings-main/src/tp1/logic/GameObjectContainer.java
@@ -1,6 +1,28 @@
 package tp1.logic;
 
+import tp1.logic.gameobjects.ExitDoor;
+import tp1.logic.gameobjects.Lemming;
+import tp1.logic.gameobjects.Wall;
+import java.util.ArrayList; 
+
+
 public class GameObjectContainer {
-	//TODO fill your code
+	//list of objects
+	ArrayList<Lemming> lemmingsList = new ArrayList<Lemming>();
+	ArrayList<Wall> wallsList = new ArrayList<Wall>();
+	ArrayList<ExitDoor> exitDoorsList = new ArrayList<ExitDoor>();
+
+	public void add(Lemming lemming) {
+		lemmingsList.add(lemming);
+	}
+
+    public void add(Wall wall) {
+		wallsList.add(wall);
+	}
+
+	public void add(ExitDoor exitDoor) {
+		exitDoorsList.add(exitDoor);
+	}
+
 
 }
